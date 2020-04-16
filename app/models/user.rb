96733b1f95dc6d797874dd3da7c4e80b076fe8a2
andrew041994 <andrew041994@gmail.com>
2020-04-16 02:18:21 -0400

    refactor sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+
+  belongs_to :company
+  has_many :clients
+  has_many :properties, through: :company
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         has_many :clients
-         belongs_to :company
-         has_many :properties, through: :company
 end
